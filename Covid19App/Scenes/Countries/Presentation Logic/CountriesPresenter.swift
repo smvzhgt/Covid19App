dd8f3a13392c904f2951a83d320c945be458f367
Oleg <smvzhgt@gmail.com>
2020-09-26 15:28:37 +0300

    refactoring - split presenter result

--- Covid19App/Scenes/Countries/Presentation Logic/CountriesPresenter.swift
+++ Covid19App/Scenes/Countries/Presentation Logic/CountriesPresenter.swift
@@ -21,21 +21,29 @@ extension CountriesPresenter: CountriesPresenterProtocol {
     func presentFetchCountriesInformation(response: Countries.Fetch.Response) {
         switch response.result {
         case .success(let models):
-            DispatchQueue.global(qos: .userInitiated).async {  [weak self] in
-                guard let `self` = self else { return }
-                let presentationModels = models.map { caseModel -> Countries.CountryPresentationModel in
-                    return Countries.CountryPresentationModel(caseModel: caseModel)
-                }
-                DispatchQueue.main.async { [weak self] in
-                    guard let `self` = self else { return }
-                    let viewModel = Countries.Fetch.ViewModel(models: presentationModels, error: nil)
-                    self.view.displayFetchCountriesInformation(viewModel: viewModel)
-                }
-            }
+            handleSuccessResult(models: models)
         case .failure(let error):
-            let viewModel = Countries.Fetch.ViewModel(models: [], error: error)
-            view.displayFetchCountriesInformation(viewModel: viewModel)
+            handleFailureResult(error: error)
         }
+    }
+    
+    private func handleSuccessResult(models: [CaseModel]) {
+        DispatchQueue.global(qos: .userInitiated).async {  [weak self] in
+            guard let `self` = self else { return }
+            let presentationModels = models.map { caseModel -> Countries.CountryPresentationModel in
+                return Countries.CountryPresentationModel(caseModel: caseModel)
+            }
+            DispatchQueue.main.async { [weak self] in
+                guard let `self` = self else { return }
+                let viewModel = Countries.Fetch.ViewModel(models: presentationModels, error: nil)
+                self.view.displayFetchCountriesInformation(viewModel: viewModel)
+            }
+        }
+    }
+    
+    private func handleFailureResult(error: CommonError) {
+        let viewModel = Countries.Fetch.ViewModel(models: [], error: error)
+        view.displayFetchCountriesInformation(viewModel: viewModel)
     }
     
 }
